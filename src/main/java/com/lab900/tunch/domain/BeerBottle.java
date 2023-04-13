c98ffcb1901928dcaeb839e22a533abee67e3200
Luís Santos <49029465+lab900-luis-santos@users.noreply.github.com>
2023-04-13 12:27:16 +0200

    feat: add extra check to beer bottle

--- src/main/java/com/lab900/tunch/domain/BeerBottle.java
+++ src/main/java/com/lab900/tunch/domain/BeerBottle.java
@@ -61,6 +61,10 @@ public class BeerBottle implements Serializable {
         this.expirationDate = expirationDate;
     }
 
+    public boolean isExpired() {
+        return LocalDate.now().isAfter(this.expirationDate);
+    }
+
     public Beer getBeer() {
         return this.beer;
     }
